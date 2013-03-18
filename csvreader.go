01bf93edfaee89826b3f8b29a0c14cda4020e42c
Alejandro Gaviria <mr.gaviria@gmail.com>
2013-03-18 19:30:40 +0000

    Fixed error control to use built-in panic err

--- csvreader.go
+++ csvreader.go
@@ -10,12 +10,11 @@ import (
 // documentation for csv is at http://golang.org/pkg/encoding/csv/
 // TODO; could not find
 func main() {
-	file, error := os.Open("csvdata/csvtest.csv")
-	if error != nil {
+	file, err := os.Open("csvdata/csvtest.csv")
+	if err != nil {
 		// error is printable
 		// element passed are separated by space automatically
-		fmt.Println("Error: ", error)
-		return
+		panic(err)
 	}
 	// automatically call Close() at the end of current method
 	defer file.Close()
@@ -27,13 +26,12 @@ func main() {
 	lineCount := 1
 	for {
 		// read just one record, but we could ReadAll() as well
-		record, error := reader.Read()
+		record, err := reader.Read()
 		// EOF is fitted into error
-		if error == io.EOF {
+		if err == io.EOF {
 			break
-		} else if error != nil {
-			fmt.Println("Error:", error)
-			return
+		} else if err != nil {
+			panic(err)
 		}
 		// record is an array of string so is directly printable
 		fmt.Println("Record", lineCount, "is", record, "and has", len(record), "fields")
